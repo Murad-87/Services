804c39ff850b12fe8c8533f4de2ace2087e4c3c5
Murad <m.iusupov>
2022-11-08 00:38:46 +0300

    getting to know notifications

--- app/src/main/java/com/example/myservices/MainActivity.kt
+++ app/src/main/java/com/example/myservices/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.myservices
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import com.example.myservices.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var id = 0
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
@@ -17,5 +24,36 @@ class MainActivity : AppCompatActivity() {
         binding.simpleService.setOnClickListener {
             startService(MyService.newIntent(this, 25))
         }
+
+        binding.foregroundService.setOnClickListener {
+            showNotification()
+        }
+    }
+
+    private fun showNotification() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("MyServices")
+            .setContentText("Do you know that you have a service running?")
+            .setSmallIcon(R.drawable.ic_start_services)
+            .build()
+
+        notificationManager.notify(id++, notification)
+    }
+
+    companion object {
+
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
     }
 }
