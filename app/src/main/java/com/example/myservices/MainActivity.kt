25398000ab21319dad03f2d03feeae546c9b629b
Murad <m.iusupov>
2022-11-08 00:05:44 +0300

    start commit

---
+++ app/src/main/java/com/example/myservices/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myservices
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myservices.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.simpleService.setOnClickListener {
+            startService(MyService.newIntent(this, 25))
+        }
+    }
+}
